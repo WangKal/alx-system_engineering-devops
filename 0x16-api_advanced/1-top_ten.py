f63ae2a2f68336f5041d67616add55a05c764818
WangKal <edgarsam.otieno@gmail.com>
2024-08-15 14:09:29 -0700

    m

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,40 +1,20 @@
 #!/usr/bin/python3
-"""Module that consumes the Reddit API and prints the titles of the first
-10 hot posts listed for a given subreddit."""
+"""A function that queries the Reddit API and prints the titles of the first 10
+hot posts listed for a given subreddit"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Queries the Reddit API and prints the titles of the first 10 hot
-    posts listed for a given subreddit.
-
-    If not a valid subreddit, print None.
-    Invalid subreddits may return a redirect to search results. Ensure
-    that you are not following redirects.
-
-    Args:
-        subreddit (str): subreddit
-
-    Returns:
-        str: titles of the first 10 hot posts
-    """
-    base_url = 'https://www.reddit.com'
-    sort = 'top'
-    limit = 10
-    url = '{}/r/{}/.json?sort={}&limit={}'.format(
-        base_url, subreddit, sort, limit)
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
-    }
-    response = requests.get(
-        url,
-        headers=headers,
-        allow_redirects=False
-    )
-    if response.status_code == 200:
-        for post in response.json()['data']['children'][0:10]:
-            print(post['data']['title'])
-    else:
+    """prints the titles of the first 10 hot posts listed"""
+    r = requests.get(r'https://www.reddit.com/r/{}/hot/.json'
+                     .format(subreddit), headers={'User-agent': 'x'},
+                     allow_redirects=False)
+    if r.status_code != 200:
         print(None)
+        return
+    l = r.json().get('data').get('children')
+    for data in l[:10]:
+        da = data['data']
+        title = da.get('title')
+        print(title)
